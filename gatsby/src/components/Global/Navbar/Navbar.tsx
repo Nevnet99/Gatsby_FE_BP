e4ec3715013ea79050f8e2d762a4a5b654aa152e
LukeBrannagan <luke.brannagan@deptagency.com>
2021-01-23 19:13:18 +0000

    feat: Implemented the Navbar, also implemented icon component to deal with icons in the proj.

--- gatsby/src/components/Global/Navbar/Navbar.tsx
+++ gatsby/src/components/Global/Navbar/Navbar.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react"
+import Icon from "../../Shared/Icon/Icon"
+import {
+  Buttons,
+  Checkout,
+  Logo,
+  NavbarItems,
+  NavButton,
+  NavWrapper,
+} from "./Navbar.styles"
 
 export const Navbar = () => (
-  <div>
-    <h1>Navbar</h1>
-  </div>
+  <NavWrapper>
+    <NavbarItems>
+      <Logo>Kicks.</Logo>
+      <Buttons>
+        <NavButton>Shoes</NavButton>
+        <NavButton>Clothing</NavButton>
+      </Buttons>
+      <Checkout>
+        <p>View cart</p>
+        <Icon icon="cart" />
+      </Checkout>
+    </NavbarItems>
+  </NavWrapper>
 )
