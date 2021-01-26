8d65a991d37e5797800731ae39b36be7c1999021
LukeBrannagan <luke.brannagan@deptagency.com>
2021-01-26 11:26:40 +0000

    feat: implemented the pages with some fixes as well, Carousel has been put on hold till I have some of the correct size images.

--- gatsby/src/components/Global/Layout/Layout.tsx
+++ gatsby/src/components/Global/Layout/Layout.tsx
@@ -1,25 +1,19 @@
-import React from "react"
+import React, { FC } from "react"
 import { ThemeProvider } from "styled-components"
 import { GlobalStyle } from "../../../styles/global"
 import { theme } from "../../../styles/theme"
-import { Footer } from "../Footer/Footer"
-import { Navbar } from "../Navbar/Navbar"
+import Footer from "../Footer/Footer"
+import Navbar from "../Navbar/Navbar"
 
-interface Props {
-  children?: React.ReactNode
-}
-
-const Layout: React.FC<Props> = ({ children }: Props) => {
-  return (
-    <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />)
-        <Navbar />
-        {children}
-        <Footer />
-      </ThemeProvider>
-    </>
-  )
-}
+const Layout: FC = ({ children }) => (
+  <>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Navbar spacer={true} />
+      {children}
+      <Footer />
+    </ThemeProvider>
+  </>
+)
 
 export default Layout
--- gatsby/src/pages/index.tsx
+++ gatsby/src/pages/index.tsx
@@ -2,11 +2,10 @@ import React from "react"
 import Layout from "../components/Global/Layout/Layout"
 import Carousel from "../components/Global/Carousel/Carousel"
 import HalfBanner from "../components/Global/HalfBanner/HalfBanner"
-import { ProductBanner3Col } from "../components/Global/ProductBanner3Col/ProductBanner3Col"
+import ProductBanner3Col from "../components/Global/ProductBanner3Col/ProductBanner3Col"
 
 const IndexPage: React.FC = () => (
   <Layout>
-    <Carousel />
     <HalfBanner />
     <ProductBanner3Col />
   </Layout>
